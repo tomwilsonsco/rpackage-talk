a4d902e7a21e685eee2b453df49bb8afb830a6e3
tomwilsonsco <thomaswilson81@gmail.com>
2022-08-18 16:14:54 +0100

    updates so far

--- process_run_through.R
+++ process_run_through.R
@@ -1,3 +1,36 @@
 library(devtools)
 # Create a package structure in a directory
 create_package("C:/repos/rpackagestest")
+
+# when in the package run
+use_git()
+
+#create a function
+use_r()
+
+# then we want to create a function
+# two examples
+
+standard_scale <- function(x){
+  (x - mean(x)) / sd(x)
+}
+
+scale_column <- function(df, col){
+ dplyr::mutate(df,  "scaled_{{col}}":= standard_scale({{col}}))
+}
+
+#(talk about help functions and main function, which to comment)
+
+# The roxygen comment should be added for the main function and then create documentation and namespace entry
+document()
+
+#Now test it
+load_all()
+
+#(Note that scale_column function available, but not in global env because it's like loading from library()
+
+#Good idea to check package periodically
+check()
+
+# If want to use a pipe
+use_pipe()
