a16034e957d8575c1ca2ea3742c1803581b4267e
tomwilsonsco <thomaswilson81@gmail.com>
2022-09-30 09:36:00 +0100

    edits to all

--- process_run_through.R
+++ process_run_through.R
@@ -1,46 +1,50 @@
 
-# Create a package structure in a directory
+# Create a package structure in a directory - here rpackagestest
 usethis::create_package("C:/tests/rpackagestest")
 
-#Note rules on creating a package (no special cr)
+# Note rules on naming a package (letters, numbers, . only, at least 2 letters, start with letter)
 
 # when in the package run
 usethis::use_git() # might be better to use git init as with older usethis default is named master, not main
-# Can use Git Bash / shell 
+# Can use Git Bash / shell
 # to change default branch to main
-# git branch -m master main 
+# git branch -m master main
 # git remote add origin ssh_to_remote
 
-#create a function
+# create a function
 usethis::use_r("std_scale_column")
 
 # then we want to create a function
-std_scale <- function(x){
+std_scale <- function(x) {
   (x - mean(x)) / stats::sd(x)
 }
 
-std_scale_column <- function(df, col){
- dplyr::mutate(df,  "{{col}}_scaled" := std_scale({{col}}))
+std_scale_df <- function(df) {
+  df %>% dplyr::mutate_if(is.numeric, std_scale)
 }
 
-#(talk about help functions and main function, which to comment)
+# (talk about help functions and main function, which to comment)
 # Add dplyr to imports
 usethis::use_package("dplyr")
 usethis::use_package("stats")
 
 # The roxygen comment should be added for the main function and then create documentation and namespace entry
-devtools::document() #equivalent of roxygen2::roxygenize()
+devtools::document() # equivalent of roxygen2::roxygenize()
 
-#Now let's test it!
+# Now let's test it!
 usethis::load_all()
 
 test_iris <- head(iris, 10)
-std_scale_column(test_iris, Sepal.Length)
-#(Note that scale_column function available, but not in global env because it's like loading from library()
-#Bring up the help
-?std_scale_column
+std_scale_df(test_iris)
+#Show that it's worked by printing original and scaled
+head(iris, 10)
+test_iris
 
-#Good idea to check package periodically while developing
+# (Note that scale_column function available, but not in global env because it's like loading from library()
+# Bring up the help
+?std_scale_df
+
+# Good idea to check package periodically while developing
 usethis::check()
 
 # Find we are getting a warning due to the :=
@@ -54,7 +58,7 @@ usethis::use_pipe()
 
 # Final thing we will cover - building
 usethis::build() # this will create a "source" tar.gz
-usethis::build(binary=TRUE) # this will make a compiled R binary at the version of R you are using
+usethis::build(binary = TRUE) # this will make a compiled R binary at the version of R you are using
 
 # If want to change the build version - edit the description file or
 usethis::use_version()
